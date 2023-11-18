a1a3dca61ff9841d589287c7a4b7e1176455bb05
priyanshagra <127022024+priyanshagra@users.noreply.github.com>
2023-11-18 23:37:28 +0530

    checkedtillnow

--- income.js
+++ income.js
@@ -13,14 +13,6 @@ exports.addIncome = async (req, res) => {
 
 
   try {
-    if(!title || !category || !description || !date)
-    {
-      return res.status(400).json({message: 'All fields are required'})
-    }
-  if(amount<=0 || !amount === 'number')
-    {
-      return res.status(400).json({message: 'Amount must be positive'})
-    }
     await income.save()
     res.status(200).json({success:true})
   } catch (error) {
